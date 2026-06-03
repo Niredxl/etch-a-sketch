81de61ec2865158143fa0f2ead304ad800c23b08
Niredxl <119158706+Niredxl@users.noreply.github.com>
2026-06-03 12:47:28 +0530

    grid boxes change color when hovered by mouse

--- script.js
+++ script.js
@@ -6,9 +6,17 @@ gridrow.classList.add("gridrow");
 
 const gridbox = document.createElement("div");
 gridbox.classList.add("gridbox");
+
+// hover effect for the boxes
+grid.addEventListener("mouseover", () => {
+            event.target.style.backgroundColor = 'green';
+        });
+
+// adding grid boxes to a row
 for (let i = 0; i < gridsize; i++){
     gridrow.appendChild(gridbox.cloneNode(1));
 }
+// adding rows to the grid
 for (let i = 0; i < gridsize; i++){
     grid.appendChild(gridrow.cloneNode(1));
 }
