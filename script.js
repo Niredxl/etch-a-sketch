188060408e3e238bb538663bcb500b21c6111686
Niredxl <119158706+Niredxl@users.noreply.github.com>
2026-06-03 13:13:40 +0530

    added a button to change grid size

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
-console.log("Hello World");
-let gridsize = 16;
 const grid = document.querySelector(".grid");
-const gridrow = document.createElement("div");
-gridrow.classList.add("gridrow");
 
-const gridbox = document.createElement("div");
-gridbox.classList.add("gridbox");
+function createGrid(gridsize){
+    
+    const gridrow = document.createElement("div");
+    gridrow.classList.add("gridrow");
 
-// hover effect for the boxes
-grid.addEventListener("mouseover", () => {
-            event.target.style.backgroundColor = 'green';
-        });
+    const gridbox = document.createElement("div");
+    gridbox.classList.add("gridbox");
+    gridbox.style.backgroundColor = "white";
 
-// adding grid boxes to a row
-for (let i = 0; i < gridsize; i++){
-    gridrow.appendChild(gridbox.cloneNode(1));
+    // hover effect for the boxes
+    grid.addEventListener("mouseover", () => {
+                event.target.style.backgroundColor = 'green';
+            });
+
+    // adding grid boxes to a row
+    for (let i = 0; i < gridsize; i++){
+        gridrow.appendChild(gridbox.cloneNode(1));
+    }
+    // adding rows to the grid
+    for (let i = 0; i < gridsize; i++){
+        grid.appendChild(gridrow.cloneNode(1));
+    }
 }
-// adding rows to the grid
-for (let i = 0; i < gridsize; i++){
-    grid.appendChild(gridrow.cloneNode(1));
+createGrid(16);
+function deleteGrid(parent){
+    while (parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
 }
+
+const button = document.querySelector(".btn");
+const line = document.getElementById("line");
+button.addEventListener("click", () => {
+    let user_input = prompt("Enter a grid size (max 100) : ", "16");
+    let user_number = parseInt(user_input);
+    if (user_input == null || user_number > 100){
+        text = "no valid input given";
+        line.innerHTML = text;
+    } else{
+        text = "Hover over a box to change color";
+        line.innerHTML = text;
+        deleteGrid(grid);
+        createGrid(user_number);
+    }
+});
+
+
+
+
